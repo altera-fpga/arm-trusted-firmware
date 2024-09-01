0f5ad6f2634e7c700d307759f6d3858fc2de4432
Sieu Mun Tang <sieu.mun.tang@intel.com>
2024-09-01 08:53:59 +0800

    fix(intel): handle cold reset via physical reset switch
    On the Agilex5 platform when cold reset is issued via
    CLI application in the OS, it is received in the BL31
    via a SMC call and handled accordingly like flush/invalidate
    the caches. However, when the cold reset is issued via an
    external switch these handlings are missed.
    This patch addresses those missed cache operations.
    
    Signed-off-by: Girisha Dengi <girisha.dengi@intel.com>

--- plat/intel/soc/agilex5/bl31_plat_setup.c
+++ plat/intel/soc/agilex5/bl31_plat_setup.c
@@ -199,6 +199,9 @@ void bl31_plat_arch_setup(void)
 	NOTICE("SOCFPGA: CPU ID = %x\n", cpuid);
 	INFO("SOCFPGA: Invalidate Data cache\n");
 	invalidate_dcache_all();
+
+	/* Invalidate for NS EL2 and EL1 */
+	invalidate_cache_low_el();
 }
 
 /* Get non-secure image entrypoint for BL33. Zephyr and Linux */
